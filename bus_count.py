32eb504add6956ef36acf9bffc6cb9179a15f7a2
Vikhram5 <126406451+Vikhram5@users.noreply.github.com>
2024-02-29 09:26:57 +0530

    Add files via upload

---
+++ bus_count.py
@@ -0,0 +1,37 @@
+# 519 bus counts w.r.t. Start time
+
+import pandas as pd
+df=pd.read_csv('519.csv')
+print(len(df))
+
+cols=['Schedule Name','Trip Start Time','Source','Destination']
+buses_df=df[cols]
+
+#transform to another csv file
+buses_df.to_csv('bus_counts.csv')
+
+df=pd.read_csv('bus_counts.csv')
+
+# Hour time 
+
+df['Trip Start Time'] = pd.to_datetime(df['Trip Start Time'], format='%H:%M:%S')
+df['Hour']=df['Trip Start Time'].dt.hour
+
+cols=['Schedule Name','Hour']
+df_final=df[cols]
+df_final.head()
+
+# Bus counts
+bus_df=df.groupby('Hour')['Schedule Name'].count().reset_index()
+bus_df.columns=['Hour','Bus Count']
+bus_df
+
+print(bus_df['Bus Count'].sum())
+
+#transform to final csv
+bus_df.to_csv('519_bus_count.csv')
+
+
+
+
+
